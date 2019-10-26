f28fd2ffc717c1dfec2573219345c546597bb581
snufka <52801250+snufka@users.noreply.github.com>
2019-10-26 21:12:17 +0200

    Delete logic.js

--- logic.js
+++
@@ -1,9 +0,0 @@
-
-
-for (var i = 0; i < 8; i++) {
-    var hoursEL = $("<input>");
-    hoursEL.addClass("hours");
-    hoursEL.attr("data-hour", i + 9);
-    $(".container").append(hoursEL);
-    console.log(i + 9);
-}
